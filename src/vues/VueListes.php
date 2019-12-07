36d52f86aa042c264746fcbef0f54a7d311a1b3e
Gauthier Mayer <legrangm@gmail.com>
2019-12-07 21:55:45 +0100

    Correction VueListes

--- src/vues/VueListes.php
+++ src/vues/VueListes.php
@@ -20,34 +20,45 @@ class VueListes
     }
 
     public function afficher(){
-        $head = "<!doctype html>
-                    <html lang=\"fr\">
-                        <head>
-                            <meta charset=\"UTF-8\">
-                            <meta name=\"viewport\"
-                                  content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">
-                            <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">
-                            <title>Items de la liste </title>
-                        </head>
-                        <body>";
+        echo
+<<<END
+<!doctype html>
+    <html lang=\"fr\">
+        <head>
+            <meta charset=\"UTF-8\">
+            <meta name=\"viewport\" content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">
+            <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">
+            <LINK href="styles/view-listes.css" rel="stylesheet" type="text/css">
+            <title>Toutes les wishlist </title>
+        </head>
+        <body>
+END;
 
-        $footer = "</body>
-                    </html>";
-
-        $content = "";
         if ($this->liste != NULL){
             foreach ($this->liste as $key => $l){
                 //$rootUri = $this->app->request->getRootUri() ;
                 $listeUrl = $this->app->urlFor('route_liste', ['id_liste'=> $l['no']] ) ;
+                $num = $l['no'];
+                $titre = $l['titre'];
+                $desc = $l['description'];
+                $user_id = $l['user_id'];
+                
+                echo
+<<<END
+        <div class="liste">
+            <a href=" $listeUrl">  $num . $titre : $desc par $user_id . </a>
+        </div>
 
-                $content .= '
-                            <div class="liste">
-                                <a href="'.$listeUrl.'"> ' . $l['no'] . '. '. $l['titre'] . ' : ' . $l['description'] . ' par ' . $l['user_id'] . '</a>
-                            </div>';
+END;
+
             }
         }
-        echo $head.$content.$footer;
+
+        echo
+<<<END
+        </body>
+    </html>
+END;
+
     }
-
-
 }
